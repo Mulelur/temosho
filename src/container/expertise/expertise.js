e7c2a8945b6a5f8ae0221bf79d1c5706528fc08d
rotonda <rotondwamulelu@gmail.com>
2021-06-04 19:50:19 +0200

    add subject set

--- src/container/expertise/expertise.js
+++ src/container/expertise/expertise.js
@@ -23,7 +23,7 @@ export default function ExpertiseContainer() {
         <Expertise.Group>
           <Expertise.Title>High School Subjects</Expertise.Title>
           <Expertise.List>
-            {subjectData2.map((item) => {
+            {subjectData1.map((item) => {
               return <Expertise.ListItem>{item.sbj}</Expertise.ListItem>;
             })}
           </Expertise.List>
@@ -31,7 +31,7 @@ export default function ExpertiseContainer() {
         <Expertise.Group>
           <Expertise.Title>Modules</Expertise.Title>
           <Expertise.List>
-            {subjectData1.map((item) => {
+            {subjectData2.map((item) => {
               return <Expertise.ListItem>{item.sbj}</Expertise.ListItem>;
             })}
           </Expertise.List>
